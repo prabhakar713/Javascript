f18771f637ef544df4ed4d7346502b9f0d091467
Prabhakar Kumar <prabhakar.kumar@getvymo.com>
2022-12-28 17:49:57 +0530

    R Day-1

---
+++ R-Day-1/script.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+console.log("test");
+
+
+// variable declaration and assignment 
+var variableNAme="welcome to variable";
+console.log(variableNAme);
+//decleare varible
+let variableNAme1="welcome to variable";
+console.log(variableNAme1);
+//reaasign the variable
+variableNAme1="hello";
+console.log(variableNAme1);
+//const variable
+const variableNAme2="welcome to variable";
+console.log(variableNAme2); 
+//reaasign the variable is not avialble in const
+//variableNAme2="hello"; //error
+//console.log(variableNAme2);
+
+
